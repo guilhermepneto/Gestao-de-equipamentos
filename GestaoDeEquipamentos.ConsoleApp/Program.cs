2b0ba30f7bed450ede525b504e9c24f85cf5a932
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-05-12 21:09:03 -0300

    Add ediçao de equipamentos

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -60,6 +60,59 @@ while (true)
 
     else if (opcaoMenu == "2")
     {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Edição de Equipamentos");
+        Console.WriteLine("---------------------------------");
+
+        Console.WriteLine(
+           "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+           "Id", "Nome", "Preço de Aquisição", "Data de fabricação"
+           );
+
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamento eq = equipamentosSalvos[i];
+
+            if (eq == null)
+                continue;
+
+            Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+            eq.id, eq.nome, eq.precoAquisicao, eq.dataFabricacao
+            );
+        }
+
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Digite o ID do registro que deseja editar");
+        int idSelecionado = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o nome do equipamento: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite o preço de aquisição do equipamento: ");
+        decimal precoAquisicao = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Digite a data de fabricação do equipamento: ");
+        DateTime dataFabricacao = DateTime.Parse(Console.ReadLine());
+
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamento equipamentoSelecionado = equipamentosSalvos[i];
+
+            if (equipamentoSelecionado == null)
+                continue;
+
+            if (equipamentoSelecionado.id == idSelecionado)
+            {
+                equipamentoSelecionado.nome = nome;
+                equipamentoSelecionado.precoAquisicao = precoAquisicao;
+                equipamentoSelecionado.dataFabricacao = dataFabricacao;
+                break;
+            }
+        }
+
+        Console.WriteLine($"Equipamento {nome} foi editado com sucesso!");
+        Console.ReadLine();
     }
 
     else if (opcaoMenu == "3")
